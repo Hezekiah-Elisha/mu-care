cc8d467abb91b6dba7f3cdda38cd31423fe71ac7
gichukibrian <64812737+brian23mwangi@users.noreply.github.com>
2023-03-18 17:52:51 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+<footer>
+	<div class="footer-logo">
+		MOI UNIVERSITY CARE<i class="fas fa-stethoscope"></i>
+	</div>
+	<div class="social">
+		<a href="#"><i class="fab fa-facebook"></i></a>
+		<a href="#"><i class="fab fa-twitter"></i></a>
+		<a href="#"><i class="fab fa-instagram"></i></a>
+		<a href="#"><i class="fab fa-whatsaap"></i></a>
+		<a href="#"><i class="fab fa-linkedin"></i></a>
+	</div>
+	<div class="container">
+		<div class="footer-cont">
+			<h4>ABOUT US</h4>
+				<p>
+					MU Care Teaching and Referral Hospital is committed<br> 
+					to provide quality wholistic healthcare solutions <br>
+					to all our clients at an affordable cost. <br>
+					We shall provide efficient, effective, customer<br>
+					focused and compassionate service ensuring our patients <br>
+					undergo no risk while being served in our institutions. <br>
+				</p>
+		</div>
+		<div class="footer-cont">
+			<h4>QUICK LINKS</h4>
+			<a href="">Home</a>
+			<a href="">Patient account</a>
+			<a href="">Services</a>
+			<a href="">Admin account</a>
+		</div>
+		<div class="footer-cont">
+			<h4>Newsletter</h4>
+			<p>Follow us on social media for latest updates.</p>
+			<form action="" method="">
+			<div class="footer-newsletter">
+					<input type="email" name="email" placeholder="Enter email" required>
+					<button name="subscribe">Send</button>
+			</div>
+		</form>
+		</div>
+	</div>
+	
+	<!-- <hr class="divide"> -->
+	</footer>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+	    <a href="#" class="name">MU CARE</a>
+
+	<nav class="navbar">
+		<a href="" class="active">Home</a>
+		<a href="">Services</a>
+		<a href="">About Us</a>
+		<a href="">Contact us</a>
+	</nav>
+
+	<div class="right">
+		<a href="./signup.php"><div class="far fa-user"  id=""></div></a>
+		<a href="./doc_signup.php"><div class="fas fa-user-md" id=""></div></a>
+		<div class="fa-solid fa-bars" id="menu"></div>
+	</div>
+</div>
