34c1751dffbed50b722359fe819cb0c755b9d550
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-03-31 22:36:35 +0300

    :memo: modifications of the dashboards

--- patient/send_report.php
+++ patient/send_report.php
@@ -230,20 +230,19 @@
 	<?php 
 			include '../resources/config.php';
 			if(isset($_POST['report'])){
-				$title = $_POST['title'];
-				$message = $_POST['message'];
+				$amount = $_POST['amount'];
 
-				if(empty($title)){
+				if(empty($amount)){
 
 				}else if(empty($message)){
 
 				}else{
 					$name = $_SESSION['patient'];
-					$query = "INSERT INTO report(title, message,name,date_sent) VALUES ('$title','$message','$name',NOW())";
+					$query = "INSERT INTO wallet(name,amount) VALUES ('$name','$amount')";
 					$res = mysqli_query($conn, $query);
 
 					if($res){
-						echo "<script>alert('You have sent the report sucesfully')</script>";
+						echo "<script>alert('You have desposited sucesfully')</script>";
 						header('location:./patient_index.php');
 					}
 				}
@@ -251,10 +250,8 @@
 		?>
 	<div class="report">
 		<form method="post">
-			<h3>Send Report</h3>
-			<input type="text" name="title" class="form-control" placeholder="enter report title" required>
-
-			<input type="text" name="message" placeholder="enter message" class="form-control" required>
+			<h3>DEPOSIT TO YOUR E-WALLET DIRECTLY</h3>
+			<input type="text" name="amount" placeholder="enter amount" class="form-control" required>
 
 			<input type="submit" class="form-btn" name="report" value="Send Report">
 		</form>
