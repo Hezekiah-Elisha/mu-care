8aa8e99f6095d9121ea66bf57fbc8ef35237b435
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-04-01 11:28:57 +0300

    :memo: modifications of the student dashboards

--- patient/patient_profile.php
+++ patient/patient_profile.php
@@ -8,8 +8,9 @@
 		<title>Patients Profile</title>
 		<!--fontawesome cdn-->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+		<link rel="stylesheet" href="./patient.css">
 	</head>
-	<style>
+	<!-- <style>
 		@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&family=Roboto:wght@100&display=swap');
 		*{
 		text-transform:uppercase;
@@ -144,25 +145,13 @@
 		h3{
 			color:#000;
 		}
-	</style>
+	</style> -->
 	
 	<body>
-
-		<div class="side-menu">
-	<div class="brand-name">
-		<h1>PATIENT</h1>
-	</div>
-	<ul>
-		<li><a href="./patient_index.php" style="color:#fff;text-decoration:none;"><i class="far fa-clipboard"></i>&nbsp;Dashboard</a></li>
-		<li><a href="./patient_profile.php" style="color:#fff;text-decoration:none;"><i class="far fa-address-card"></i>&nbsp;my Profile</a></li>
-		<li><a href="./appointment.php" style="color:#fff;text-decoration:none;"><i class="far fa-calendar"></i>&nbsp;Book Appointment</a></li>
-		<li><a href="./view_appointment.php" style="color:#fff;text-decoration:none;"><i class="far fa-calendar"></i>&nbsp;My Appointment</a></li>
-		<li><a href="./invoice.php" style="color:#fff;text-decoration:none;"><i class="fas fa-book-reader"></i>&nbsp;my Invoice</a></li>
-		<li><a href="./send_report.php" style="color:#fff;text-decoration:none;"><i class="fas fa-bell"></i>&nbsp;Send Reports</a></li>
-		<li><a href="./view_report.php" style="color:#fff;text-decoration:none;"><i class="fas fa-bell"></i>&nbsp;View Reports</a></li>
-		<li><a href="./logout.php" class="logout" style="color:#fff;text-decoration:none;"><i class="fas fa-sign-out-alt"></i>&nbsp;Logout</a></li>
-	</ul>
-</div>
+		
+		<?php
+			include_once 'patient_side_menu.php';
+		?>
 
 <div class="container">
 	<div class="header">
